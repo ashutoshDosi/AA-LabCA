96f0c012e90327969930bf18c867edfdf6a5cdc7
Ashutosh Rakesh Dosi <ashutoshdosi@gmail.com>
2025-04-02 23:57:23 +0530

    Change title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-'use client";'
+'use client'
 import Image from "next/image";
 import QuickSortVisualizer from "./components/QuickSortVisualizer";
+import Head from "next/head";
+import { use, useEffect } from "react";
 
 export default function Home() {
   return (
+    
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div>
@@ -14,8 +17,8 @@ export default function Home() {
             By: <br />
             16010423002: Aashna Parikh <br />
             16010423018: Ashutosh Dosi <br />
-            16010423032 - Divyesh Shirva <br />
-            16010423034 - Gargi Joshi
+            16010423032: Divyesh Shirva <br />
+            16010423034: Gargi Joshi
           </p>
         </div>
         <div className="flex flex-col items-center ml-8">
